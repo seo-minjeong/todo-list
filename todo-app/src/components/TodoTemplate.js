5523cb92065400ec946d9c23491e8b8c59b64520
seominjeong <smj12938@gmail.com>
2022-06-21 23:59:35 +0900

    Design: background , input

--- todo-app/src/components/TodoTemplate.js
+++ todo-app/src/components/TodoTemplate.js
@@ -4,7 +4,7 @@ import "./TodoTemplate.scss";
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
-      <div className="app-title">All Task</div>
+      <div className="app-title">All Task ✏️</div>
       {/* content 안에 todo content를 담아줌. */}
       <div className="content">{children}</div>
     </div>
