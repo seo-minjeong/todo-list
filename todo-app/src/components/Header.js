3555eefc3ec315509f1c402ac8ced59852c63977
seominjeong <smj12938@gmail.com>
2022-06-21 18:36:59 +0900

    style: main css

---
+++ todo-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="logo">T</h1>
+      <h2 className="logo_text">Todo List</h2>
+    </header>
+  );
+};
+
+export default Header;
