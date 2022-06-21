5d5f20c49a37deea3aecf2ff5d50690e7166ce03
seominjeong <smj12938@gmail.com>
2022-06-21 19:39:38 +0900

    Feat: todo add , remove , edit

--- todo-app/src/components/TodoTemplate.js
+++ todo-app/src/components/TodoTemplate.js
@@ -5,6 +5,7 @@ const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
       <div className="app-title">일정 관리</div>
+      {/* content 안에 todo content를 담아줌. */}
       <div className="content">{children}</div>
     </div>
   );
